d3e4312816dda6a6ca06b5b5a6f2120ac7c2860c
_____wj <wj122791@gmail.com>
2022-01-15 18:33:04 +0900

    first commit

---
+++ WJCalendarModule/src/main/java/com/jay/wjcalendarmodule/CalendarTest.kt
@@ -0,0 +1,12 @@
+package com.jay.wjcalendarmodule
+
+import android.content.Context
+import android.widget.Toast
+
+class CalendarTest {
+
+    fun getTest(): String = "test"
+
+    fun test(context: Context) = Toast.makeText(context, "good", Toast.LENGTH_SHORT).show()
+
+}
---
+++ app/src/main/java/com/jay/wjcalendar/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.jay.wjcalendar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.jay.wjcalendarmodule.CalendarTest
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        CalendarTest().test(this)
+    }
+}
